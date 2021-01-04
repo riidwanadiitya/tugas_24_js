81e865712b60aaac3fd51aa465da7352b0c62191
Ridwan <71011389+riidwanadiitya@users.noreply.github.com>
2021-01-04 09:40:27 +0700

    TUGAS 24 JS

---
+++ tugas24.js
@@ -0,0 +1,9 @@
+function tugas24() {
+    var bilangan = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+    console.log("Sebelumnya : " + bilangan);
+    dariTerkecil = bilangan.sort();
+    console.log("Ascending : " + dariTerkecil);
+    dariTerbesar = bilangan.reverse();
+    console.log("Descending : " + dariTerbesar)
+}
+tugas24();
